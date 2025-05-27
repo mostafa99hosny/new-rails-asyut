f261392fbd92ae07aa68282d19b748228c73bda4
mostafa99hosny <m.hosney33@gmail.com>
2025-05-27 06:55:03 +0300

    updated routes.rb adding resources :posts to handle all routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  # get "posts/index"
+  # get "posts/show"
+  # get "posts/new"
+  # get "posts/create"
+  # get "posts/edit"
+  # get "posts/update"
+  # get "posts/destroy"
+  resources :posts
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -11,5 +19,5 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
-  get "/posts", to: "posts#index"
+ 
 end
