2c30705d1bb6383903727a776fde1777ba7e7eee
mostafa99hosny <m.hosney33@gmail.com>
2025-05-27 07:30:48 +0300

    adding User model

---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ApplicationRecord
+  validates :name, presence: true
+  validates :DOB, presence: true
+  validates :email, presence: true, uniqueness: true, format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i, message: "must be a valid email address" }
+  validates :phone_number, presence: true
+  validates :address, presence: true
+end
