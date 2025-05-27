4838853084a84479cc0a671c16a5bca648de5c9f
mostafa99hosny <m.hosney33@gmail.com>
2025-05-27 07:28:27 +0300

    updated routes.rb to route to users

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   # get "posts/update"
   # get "posts/destroy"
   resources :posts
+  resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
